d531cbe57774fd3699464b8a06034914b1917179
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2022-09-29 17:52:02 +0530

    Angular Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialDesignModule } from "./material-design/material-design.module";
+
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
+    MaterialDesignModule,
     BrowserAnimationsModule
   ],
   providers: [],
